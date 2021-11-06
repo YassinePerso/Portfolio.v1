2689835046752d8bc448882128438d3e3004540c
yassine <yassineboulakhrif75@gmail.com>
2021-11-06 22:37:39 +0100

    First commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../Components/Header.css'
+
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Accueil</li>
+                    <li>Qui suis-je ?</li>
+                    <li>Projets</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
