a6e53fc3e207b208a0b5e0effc7bf12e45e0f78f
yassine <yassineboulakhrif75@gmail.com>
2021-11-07 23:54:02 +0100

    Added some GSAP animation

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import Header from '../src/Components/Header';
 import Title from '../src/Components/Title'
 import Footer from '../src/Components/Footer'
+// import Cursor from './Components/Cursor';
 
 function App() {
   return (
     <div className="App">
+      {/* <Cursor /> */}
       <Header />
       <Title />
       <Footer />
--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,21 +1,132 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import '../Components/Title.css'
+import { gsap } from 'gsap';
+// import { TweenLite } from 'gsap/TweenLite';
+
 
 const Title = () => {
+
+    const yassineTitle = useRef(null)
+    const webdevTitle = useRef(null)
+    const frontendTitle = useRef(null)
+    const reactTitle = useRef(null)
+    const line1 = useRef(null)
+    const line2 = useRef(null)
+    const line3 = useRef(null)
+
+    useEffect(() => {
+        gsap.from(yassineTitle.current, {
+            opacity: 0,
+            y: 80,
+    }
+    )
+        gsap.to(yassineTitle.current, {
+                opacity: 1,
+                duration: 3,
+                y: 0,
+        }
+        )
+    }, [])
+
+    useEffect(() => {
+        gsap.from(webdevTitle.current, {
+            opacity: 0,
+            y: 80,
+    }
+    )
+        gsap.to(webdevTitle.current, {
+                opacity: 1,
+                duration: 2,
+                y: 0,
+        }
+        )
+    }, [])
+
+    useEffect(() => {
+        gsap.from(frontendTitle.current, {
+            opacity: 0,
+            y: 80,
+    }
+    )
+        gsap.to(frontendTitle.current, {
+                opacity: 1,
+                duration: 1,
+                y: 0,
+        }
+        )
+    }, [])
+
+    useEffect(() => {
+        gsap.from(reactTitle.current, {
+            opacity: 0,
+            y: -100,
+            rotate: 60,
+    }
+    )
+        gsap.to(reactTitle.current, {
+                opacity: 1,
+                duration: 4,
+                y: 0,
+        }
+        )
+    }, [])
+
+    useEffect(() => {
+        gsap.from(line1.current, {
+            opacity: 0,
+            width: 0,
+    }
+    )
+        gsap.to(line1.current, {
+                opacity: 1,
+                duration: 2,
+                width: 350,
+        }
+        )
+    }, [])
+
+    useEffect(() => {
+        gsap.from(line2.current, {
+            opacity: 0,
+            width: 0,
+    }
+    )
+        gsap.to(line2.current, {
+                opacity: 1,
+                duration: 2,
+                width: 500,
+        }
+        )
+    }, [])
+
+    useEffect(() => {
+        gsap.from(line3.current, {
+            opacity: 0,
+            width: 0,
+    }
+    )
+        gsap.to(line3.current, {
+                opacity: 1,
+                duration: 2,
+                width: 350,
+        }
+        )
+    }, [])
+
     return (
         <main className="main-left">
             <section className="left-side">
             <div className="yassine">
-                <p>Yassine</p>
-                <div className="line-1"></div>
+                <p ref={yassineTitle}>Yassine</p>
+                <div ref={line1} className="line-1"></div>
             </div>
             <div className="web-dev">
-                <p>Web-dev</p>
-                <div className="line-2"></div>
+                <p ref={webdevTitle}>Web-dev</p>
+                <div ref={line2} className="line-2"></div>
             </div>
             <div className="front-end">
-                <p>Front-end</p>
-                <div className="line-3"></div>
+                <p ref={frontendTitle}>Front-end</p>
+                <div ref={line3} className="line-3"></div>
             </div>
             </section>
 
@@ -23,7 +134,7 @@ const Title = () => {
 
             <section className="right-side">
                 <div className="react-js">
-                    <p>React.js</p>
+                    <p ref={reactTitle}>React.js</p>
                 </div>
             </section>
         </main>
